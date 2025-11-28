97eb813b8334564a03382c85afacddfa09b40dab
eftekharkabir87 <eftekharkabir87@gmail.com>
2025-11-28 21:48:12 +0600

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["61572240295227"];
+    const permission = ["61575161136678"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
+    api.sendMessage("You don't have enough permission to use this command. Only My Lord Kabir Can Use It.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];
